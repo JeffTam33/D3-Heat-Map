7a295634ed029ed6106f7e10ee183c2d23ae1492
JeffTam33 <53273407+JeffTam33@users.noreply.github.com>
2022-05-19 17:37:38 -0400

    Update js.js

--- js.js
+++ js.js
@@ -1,31 +1,61 @@
 //Babel
 //Libaries: D3
 //Data used from https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json
-const width = 1200;
-const height = 600;
-const padding = 100;
+const width = 1200
+const height = 600
+const padding = 125
+const barWidth = 18
+//Colors: space cadet, celtic blue, cultured(whiteish), pale silver, red salsa, lava
+const colorHeatArr = ["#183059", "#276FBF", "#F6F4F3", "#C2B4AE", "#F03A47", "#D1101D"]
+
+//function used to sort variance temperature
+function sortVarianceByColor(temp){
+  if(temp >= 12){
+    return colorHeatArr[5]
+  }else if(temp >= 10){
+    return colorHeatArr[4]
+  }else if(temp >= 8){
+    return colorHeatArr[3]
+  }else if(temp >= 6){
+    return colorHeatArr[2]
+  }else if(temp >= 4){
+    return colorHeatArr[1]
+  }else{
+    return colorHeatArr[0]
+  }
+}
 
 var tooltips = d3.select(".visual-data")
                  .append("div")
                  .attr("id", "tooltip")
                  .attr("class", "tooltips")
-                 .style("opacity", 1);
+                 .style("opacity", 1)
 
 var svgContainer = d3.select(".visual-data")
                      .append("svg")
                      .attr("width", width)
                      .attr("height", height)
-                     .style("background-color", "#FFFFFF");
+                     .style("background-color", "#FFFFFF")
+
+var heat = svgContainer.append("g")
+                         .attr("id", "heat")
+
+var legend = svgContainer.append("g")
+                         .attr("id", "legend")
+
+var description = svgContainer.append("g")
+                         .attr("id", "description")
 
 fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/global-temperature.json")
      .then(response => response.json())
      .then(data => {
       //Making Date object to determind min and max of x axis
-      var dateArr = data.monthlyVariance.map(i => new Date(i.year, i.month));
-      var yearArr = data.monthlyVariance.map(i => i.year);
-      var monthArr = data.monthlyVariance.map(i => i.month);
+      var dateArr = data.monthlyVariance.map(i => new Date(i.year, i.month))
+      var yearArr = data.monthlyVariance.map(i => i.year)
+      var monthArr = data.monthlyVariance.map(i => i.month)
       var monthStrArr = ["January", "February", "March", "April", "May", "June", 
                          "July", "August", "September", "October", "November", "December"]
+      var varianceArr = data.monthlyVariance.map(i => i.variance)
       //Axis Scales
       const xScale = d3.scaleLinear()
                        .domain([d3.min(yearArr), d3.max(yearArr)])
@@ -33,11 +63,15 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
       const yScale = d3.scaleLinear()
                        .domain([d3.max(monthArr), d3.min(monthArr)])
                        .range([height - padding, padding])
+      const legendScale = d3.scaleLinear()
+                       .domain([d3.max(varianceArr) + data.baseTemperature, d3.min(varianceArr) + data.baseTemperature])
+                       .range([height - padding, padding])
       //Axises positions
-      const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"));
+      const xAxis = d3.axisBottom(xScale).tickFormat(d3.format("d"))
       const yAxis = d3.axisLeft(yScale).tickFormat(function (d){
         return(monthStrArr[d - 1])
-      });
+      })
+      const legendAxis = d3.axisBottom(legendScale).ticks(5);
       //Call Axises
       svgContainer.append("g")
                   .attr("id", "x-axis")
@@ -47,17 +81,50 @@ fetch("https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
                   .attr("id", "y-axis")
                   .attr("transform", "translate(" + padding +", 0)")
                   .call(yAxis)
+      svgContainer.append("g")
+                  .attr("id", "legend-axis")
+                  .attr("transform", "translate(50, 550)")
+                  .call(legendAxis)
       //x-axis title and text
       svgContainer.append("text")
                   .attr("id", "x-axis-title")
                   .attr("x", width / 2)
-                  .attr("y", height - (padding / 4))
+                  .attr("y", height - (padding / 2))
                   .text("Years")
       //Y-axis title and text
       svgContainer.append("text")
                   .attr("id", "y-axis-title")
-                  .attr("x", (height * -1) + padding * 3)
+                  .attr("x", (height * -1) + padding * 2)
                   .attr("y", padding / 3)
                   .attr("transform", "rotate(270)")
                   .text("Months")
+      //Legend-axis title and text
+      svgContainer.append("text")
+                  .attr("id", "legend-axis-title")
+                  .attr("x", width / 4)
+                  .attr("y", height)
+                  .text("Variance (C°)")
+      svgContainer.selectAll("#heat")
+                  .data(data.monthlyVariance)
+                  .enter()
+                  .append("rect")
+                  .attr("class", "cell")
+                  .attr("data-year", d => d.year)
+                  .attr("data-month", d=> d.month)
+                  .attr("data-temp", d => d.variance + data.baseTemperature)
+                  .attr("fill", d => sortVarianceByColor(data.baseTemperature + d.variance))
+                  .attr("x", d => xScale(d.year))
+                  .attr("y", d => yScale(d.month))
+                  .attr("height", "5px")  
+                  .attr("width", "5px")
+  //Legend color appear on axis
+      legend.selectAll("#legend")
+                  .data(colorHeatArr)
+                  .enter()
+                  .append("rect")
+                  .style("fill", d => d)
+                  .attr("x", (d, i) => 184 + (57 * i) + "px")
+                  .attr("y", "525px")
+                  .attr("height", "25px")  
+                  .attr("width", "56px")
      })
